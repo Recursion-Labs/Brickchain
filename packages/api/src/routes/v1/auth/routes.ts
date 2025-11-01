cb53f301c6a224214d285730c2b172143d19072f
samarth3301 <samarplayz1337@gmail.com>
2025-11-01 16:02:06 +0530

    fix(auth): streamline error handling and improve code readability in authentication controller

--- packages/api/src/routes/v1/auth/routes.ts
+++ packages/api/src/routes/v1/auth/routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/register", v1.auth.authControllers.register);
 
 router.post("/otp/send", v1.auth.authControllers.sendOtp);
+router.post("/otp/verify", v1.auth.authControllers.verifyOtp);
 
 export default router;
