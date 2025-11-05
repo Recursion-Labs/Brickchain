cd669d919c8762bad98799d45c8df2d2321acf16
samarth3301 <samarplayz1337@gmail.com>
2025-11-05 13:02:46 +0530

    Add authentication routes for Google OAuth

--- packages/api/src/routes/v1/auth/routes.ts
+++ packages/api/src/routes/v1/auth/routes.ts
@@ -9,4 +9,10 @@ router.post("/login", v1.auth.authControllers.login)
 router.post("/otp/send", v1.auth.authControllers.sendOtp);
 router.post("/otp/verify", v1.auth.authControllers.verifyOtp);
 
+router.get("/google", v1.auth.googleControllers.googleAuth);
+router.get(
+    "/google/callback",
+    v1.auth.googleControllers.googleCallback
+);
+
 export default router;
