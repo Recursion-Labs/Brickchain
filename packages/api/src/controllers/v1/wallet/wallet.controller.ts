404601c5344ade6ea10283c417b88060fe954e1b
samarth3301 <samarplayz1337@gmail.com>
2025-11-08 19:23:36 +0530

    feat: update TypeScript dependencies and improve contract compilation scripts

--- packages/api/src/controllers/v1/wallet/wallet.controller.ts
+++ packages/api/src/controllers/v1/wallet/wallet.controller.ts
@@ -1,7 +1,9 @@
 import catchAsync from "@/handlers/async.handler";
 import { Request, Response } from "express";
 
-const registerWallet = catchAsync(async (req: Request, res: Response) => {});
+const registerWallet = catchAsync(async (req: Request, res: Response) => {
+	
+});
 
 export default {
 	registerWallet,
