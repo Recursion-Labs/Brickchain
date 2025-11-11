c5e7704bf145c4b17ead086fab1d38e91caf8580
Ayush <ayushsarkar357@gmail.com>
2025-11-11 15:12:06 +0530

    feat: add cross-platform server startup scripts for Windows and Unix

---
+++ packages/storage/Start_Server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Starting BrickChain Storage Server..."
+echo ""
+echo "Database: ./.pdfdb"
+echo "Port: 3020"
+echo ""
+echo "Make sure IPFS is running on http://127.0.0.1:5001"
+echo "Make sure Substrate node is running on ws://localhost:9944"
+echo ""
+cargo run --bin store-server ./.pdfdb 3020
